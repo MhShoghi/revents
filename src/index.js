5dfb6c78ed325d7d724d299aadce96a514a41ab0
mhshoghi <hoseinshoghi@gmail.com>
2020-10-03 13:33:05 +0330

    End of section 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// External packages
+import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App.jsx";
 import * as serviceWorker from "./serviceWorker";
